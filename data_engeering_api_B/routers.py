ad4c181ca24011eec5a765be19ac78a71241af86
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-01-20 17:00:57 +0200

    add all

--- data_engeering_api_B/routers.py
+++ data_engeering_api_B/routers.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, HTTPException
-
+from schemas import WhetherLocation
+from service import clean_data
 
 
 router = APIRouter()
 
 
 @router.post("/clean")
-def clean_data(data):
-    pass
+def clean_data_route(data: WhetherLocation):
+    try:
+        return clean_data(data)
+    except Exception as e:
+        raise{"message": e}
